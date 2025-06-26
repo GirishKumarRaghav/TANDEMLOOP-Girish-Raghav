90a4a0bd34b2e5a26915efffa306b0805134ef9a
girish <graghav1142bsr@gmail.com>
2025-06-27 00:36:24 +0530

    Initial commit with Java problems and README

---
+++ Problem_2.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Problem_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int odd = 0;
+        int a = scanner.nextInt();
+        for(int i = 0; i<a; i++){
+            odd = 2*i+1;
+            System.out.println("You entered: " + odd);
+        }
+
+        scanner.close();
+    }
+}
